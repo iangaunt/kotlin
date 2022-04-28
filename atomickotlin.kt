f2fd48aadfba7c271bcab3ddb95d2d13b15825bb
Ian G <66448861+i9nn@users.noreply.github.com>
2022-04-28 09:04:21 -0700

    Create atomickotlin.kt

---
+++ atomickotlin.kt
@@ -0,0 +1,25 @@
+/* 
+    Programming Basics 
+*/
+
+/* 1. HelloWorld/Task1.kt
+   Create and run a program that displays the phrase "Hello, Kotlin!" on the console. */
+
+fun main() {
+    println("Hello, Kotlin!")
+}
+
+/* 2. HelloWorld/Task2.kt
+   Display "Hello, Kotlin!" on the console, but with "Kotlin" appearing on the next line. */
+
+fun main() {
+    println("Hello,\nKotlin!")
+}
+
+/* 3. HelloWorld/Task3.kt
+   Modify the previous exercise by commenting the line that displays "Hello". Verify that line is not executed. */
+
+fun main() {
+  // println("Hello,")
+  println("Kotlin!")
+}
